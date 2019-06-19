267bc450a91bb4e31ebd6c2167ec1852c9e22c1c
boom <564296557@qq.com>
2019-06-19 14:03:02 +0800

    所有文件提交

---
+++ vue-cource/vue.config.js
@@ -0,0 +1,19 @@
+//判断当前是在哪个环境下，如果是开发，直接指向 / ,否则指向域名
+const BASE_URL = process.env.NODE_ENV == 'procution'?'/iview-admin' :''
+
+module.exports = {
+  lintOnSave: false,
+  baseUrl:BASE_URL,
+  chainWebpack:config => {
+    config.resolve.alias
+      .set('@',resolve('src'))   //作用：使用 @ 符号代替文件夹
+      .set('_c',resolve('src/components'))
+  },
+  //打包不生成.map文件
+  procutionSourceMap:false,
+  //跨域配置
+  devServer:{
+    proxy:'http://localhost:8081'
+  }
+
+}
